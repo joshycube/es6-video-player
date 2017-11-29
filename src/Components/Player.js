f18bb9417d008c75a3afcf7043473e5097f9b7d2
jozsef <joshycube@gmail.com>
2017-11-29 12:18:48 +0000

    Player event publishing

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -5,6 +5,18 @@ import './../Features/ControlVolume';
 
 export default class Player {
   init (configObject) {
-    Feature.initiate(configObject);
+    this.events = Feature.initiate(configObject);
+    this.player = document.querySelector(configObject.videoSelector);
+    this.eventHandlers();
+  }
+
+  eventHandlers () {
+    this.events.on('play', () => {
+      this.player.play();
+    });
+
+    this.events.on('pause', () => {
+      this.player.pause();
+    });
   }
 };
--- src/Features/ControlPlay.js
+++ src/Features/ControlPlay.js
@@ -6,11 +6,26 @@ export default class ControlPlay {
     this.className = className;
     this.video = document.getElementById(config.videoSelector);
     this.controlParent = document.querySelector(`${config.parentSelector} ${config.controlSelector}`);
-    this.state = {};
+    this.state = {
+      playBtn: 'pause',
+    };
   }
 
   init () {
+    this.eventHandlers();
+  }
 
+  eventHandlers () {
+    const PlayBtn = this.controlParent.querySelector('#play-pause');
+    PlayBtn.addEventListener('click', (e) => {
+      if (this.state.playBtn === 'pause') {
+        this.events.publish('play');
+        this.state.playBtn = 'play';
+      } else {
+        this.events.publish('pause');
+        this.state.playBtn = 'pause';
+      }
+    });
   }
 
   destroy () {
